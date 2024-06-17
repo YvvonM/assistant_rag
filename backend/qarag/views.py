6b5f109ea2c17a7f915f9b4e386a1cfa5b8fdad3
YvvonM <yvvonjemmymajala@gmail.com>
2024-06-17 15:47:03 +0300

    'fixed a bug' fixed a bug

--- backend/qarag/views.py
+++ backend/qarag/views.py
@@ -4,11 +4,10 @@ from qarag.parentchild import get_answer_from_rag_chain
 import json
 
 # # Create your views here.
-def qa_form(request):
-    return render(request,'/qarag/index.html')
+# def qa_form(request):
+#     return render(request,'/qarag/index.html')
 
 def qa_rag(request):
-    
     return render(request,'/qarag/index.html')
     if request.method == "POST":
         question = request.POST.get("question")
